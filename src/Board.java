6c27e889e585d9287904f48847de78514635a638
Chris Chang <1chang1chang1@gmail.com>
2020-12-31 23:15:22 -0800

    Updated Grid Draw Method

--- src/Board.java
+++ src/Board.java
@@ -9,8 +9,13 @@ public class Board {
     private static final int GRID_ROWS = 9;
     private static final int GRID_COLS = 9;
     private static ArrayList<Character> validInputs =
-        new ArrayList<>(Arrays.asList(' ', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
+            new ArrayList<>(Arrays.asList(' ', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
 
+    /**
+     * Effect: Creates a new instance of a Board object
+     *
+     * @throws BadArgumentExpection if initialization fails due to invalid argument
+     */
     public Board() throws BadArgumentExpection {
         this.cells = new char[GRID_ROWS][GRID_COLS];
         this.solved = false;
@@ -18,31 +23,42 @@ public class Board {
     }
 
     /**
-     * initializes Board with empty cells
+     * Effect: Initializes Board with empty cells
+     *
+     * @throws BadArgumentExpection if initialization fails due to invalid argument
      */
 
     private void initializeBoard() throws BadArgumentExpection {
-        for(int c = 0; c < GRID_COLS; c++) {
-            for(int r = 0; r < GRID_ROWS; r++) {
-                this.cells[r][c] = ' ';
+        for (int c = 0; c < GRID_COLS; c++) {
+            for (int r = 0; r < GRID_ROWS; r++) {
+                this.cells[r][c] = '3';
                 if (!isValid(this.cells[r][c])) {
-                    throw new BadArgumentExpection("Invalid Argument");
+                    throw new BadArgumentExpection("Initialization failed at (" + r + ", " + c + ").");
                 }
             }
         }
     }
 
     /**
+     * Effect: Sets the index at (row, col) on the Board to the parameter argumemt specified
      *
-     * @param argument  The character representing the number to be set at cell row, col
-     * @param row       The row number in terms of Array indices
-     * @param col       The col number in terms of Array indices
+     * @param argument The character representing the number to be set at cell row, col
+     * @param row      The row number in terms of Array indices
+     * @param col      The col number in terms of Array indices
+     * @throws BadArgumentExpection if argument is invalid
      */
-    public void set(char argument, int row, int col) {
-        this.cells[row][col] = argument;
+    public void set(char argument, int row, int col) throws BadArgumentExpection {
+        if (isValid(argument)) {
+            this.cells[row][col] = argument;
+        } else {
+            throw new BadArgumentExpection("Invalid Argument " + argument + " set at (" + row + ", " + col + ").");
+        }
+
     }
 
     /**
+     * Effect: Returns the character argument at (row, col) on the Board
+     *
      * @param row The row number in terms of Array indices
      * @param col The col number in terms of Array indices
      * @return The number at the cell at row, col
@@ -52,14 +68,20 @@ public class Board {
     }
 
     /**
+     * Effect: Determines whether a character argument is valid for the Sudoku Puzzle
      *
-     * @param arg
-     * @return
+     * @param argument Any character argument
+     * @return True if argument is valid, False if argument is invalid
      */
-    public boolean isValid(char arg) {
-        return validInputs.contains(arg);
+    public boolean isValid(char argument) {
+        return validInputs.contains(argument);
     }
 
+    /**
+     * Effect: Informs caller if the puzzle is solved or unsolved
+     *
+     * @return True if solved, False if unsolved
+     */
     public boolean isSolved() {
         return this.solved;
     }
@@ -68,9 +90,9 @@ public class Board {
     public boolean equals(Object o) {
         Board board = (Board) o;
 
-        for(int c = 0; c < GRID_COLS; c++) {
-            for(int r = 0; r < GRID_ROWS; r++) {
-                if(!(this.cells[r][c] == board.get(r,c))) {
+        for (int c = 0; c < GRID_COLS; c++) {
+            for (int r = 0; r < GRID_ROWS; r++) {
+                if (!(this.cells[r][c] == board.get(r, c))) {
                     return false;
                 }
             }
--- src/Sudoku_GUI.java
+++ src/Sudoku_GUI.java
@@ -1,53 +1,93 @@
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import java.awt.BorderLayout;
+import javax.swing.*;
+import java.awt.*;
 
 public class Sudoku_GUI {
     public Board board;
-    private SudokuGrid sudokuGrid;
     private JFrame frame;
     private JLabel label;
-    private JMenu menu;
-    private static final int MY_DIMENSION = 500;
-
+    private static final int GUI_HEIGHT = 750;
+    private static final int GUI_WIDTH = 750;
+    private static final int GRID_ROWS = 9;
+    private static final int GRID_COLS = 9;
+    private static final Dimension DEFAULT_DIMENSIONS = new Dimension(GUI_WIDTH, GUI_HEIGHT);
 
     public Sudoku_GUI() throws BadArgumentExpection {
+
+
         this.board = new Board();
         this.frame = new JFrame(); // Window
         this.label = new JLabel("Hello World!");
-        this.sudokuGrid = new SudokuGrid(this.board);
-        //this.menu = new JMenu(""); // Want menu to
 
-        sudokuGrid.setBorder(
-            BorderFactory.createEmptyBorder(MY_DIMENSION, MY_DIMENSION, MY_DIMENSION,
-                MY_DIMENSION));
-        sudokuGrid.setLayout(new BorderLayout());
-        //TODO: Look into Spring Layout when adding different Solution algorithms: https://docs.oracle.com/javase/tutorial/uiswing/layout/spring.html
-
-        frame.add(sudokuGrid, BorderLayout.CENTER); // Add panel
-//        frame.add(label);
-
+        drawSudokuGrid();
+        frame.setSize(DEFAULT_DIMENSIONS);
+        frame.setPreferredSize(DEFAULT_DIMENSIONS);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Sudoku GUI");
 
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
 
     }
 
-    public void set(char argument, int row, int col) {
-        this.board.set(argument, row, col);
+    public void set(char argument, int row, int col) throws BadArgumentExpection {
+        try {
+            this.board.set(argument, row, col);
+        } catch (BadArgumentExpection e) {
+            throw new BadArgumentExpection("Invalid Argument " + argument + " set at (" + row + ", " + col + ").");
+        }
     }
 
     public char get(int row, int col) {
         return this.board.get(row, col);
     }
 
+    public static int getGuiHeight() {
+        return GUI_HEIGHT;
+    }
+
+    public static int getGuiWidth() {
+        return GUI_WIDTH;
+    }
+
+    public void drawSudokuGrid() {
+        int size = Math.min(getGuiHeight(), getGuiWidth()) / 10;
+
+        int y = (getGuiHeight() - (size * GRID_COLS)) / 2;
+
+        for (int horz = 0; horz < GRID_COLS; horz++) {
+            int x = (getGuiWidth() - (size * 10)) / 2;
+            for (int vert = 0; vert < GRID_ROWS; vert++) {
+                String text = String.valueOf(board.get(vert, horz));
+                System.out.println(text);
+                JPanel panel = new JPanel();
+                JLabel label = new JLabel(text);
+
+                label.setFont(new Font("Verdana", 1, 20));
+                panel.add(label);
+
+                panel.setBorder(BorderFactory.createLineBorder(Color.black));
+                panel.setBounds(x, y, size, size);
+
+                frame.add(panel);
+
+                x += size;
+            }
+
+            y += size;
+        }
+    }
+
+
     public static void main(String[] args) throws BadArgumentExpection {
         Sudoku_GUI s = new Sudoku_GUI();
-        s.set('9', 0, 0);
+
+        for (int c = 0; c < 9; c++) {
+            for (int r = 0; r < 9; r++) {
+                s.set('8', r, c);
+            }
+        }
     }
 
 
